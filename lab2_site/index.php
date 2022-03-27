d3a885eb8e955276b7c79b440d6f39241d38fc79
Nurmuhammad <98839225+nurmuhammadx@users.noreply.github.com>
2022-03-27 16:33:53 +0400

    add files

--- lab2_site/index.php
+++ lab2_site/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>Главная</title>
     <link rel="stylesheet" href="/styles/style.css">
-    <script type="text/javascript" src="/scripts/lab1.js"></script>
+    <script type="text/javascript" src="/scripts/script.js"></script>
 </head>
 <body>
 <p>hello world!</p>
