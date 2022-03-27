955ed16a7e3a40d68add060470cae6c1c28c367b
Nurmuhammad <98839225+nurmuhammadx@users.noreply.github.com>
2022-03-27 16:31:08 +0400

    add files

---
+++ lab2_site/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Главная</title>
+    <link rel="stylesheet" href="/styles/style.css">
+    <script type="text/javascript" src="/scripts/lab1.js"></script>
+</head>
+<body>
+<p>hello world!</p>
+</body>
+</html>
