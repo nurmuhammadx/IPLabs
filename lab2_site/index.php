1867a03d45d202818335128eb49b0f1248249e82
Nurmuhammad <98839225+nurmuhammadx@users.noreply.github.com>
2022-04-01 00:06:06 +0400

    add header

--- lab2_site/index.php
+++ lab2_site/index.php
@@ -3,10 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <title>Главная</title>
-    <link rel="stylesheet" href="/styles/style.css">
+    <link rel="stylesheet" href="styles/main.css">
     <script type="text/javascript" src="/scripts/script.js"></script>
 </head>
 <body>
-<p>hello world!</p>
+
+<header class="header">
+    <div class="container">
+        <div class="nav">
+            <img src="images/NX3.png" alt="Logo" class="logo">
+            <ul class="menu">
+                <li>
+                    <a href="#">Главная</a>
+                </li>
+                <li>
+                    <a href="#">Мои работы</a>
+                </li>
+                <li>
+                    <a href="#">Галерея</a>
+                </li>
+                <li>
+                    <a href="#">О себе</a>
+                </li>
+                <li>
+                    <a href="#">Контакты</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</header>
 </body>
 </html>
