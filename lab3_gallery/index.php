24dd9b45a62ab3b0a4f2fce787a92d54613a8e1e
Nurmuhammad <98839225+nurmuhammadx@users.noreply.github.com>
2022-04-15 19:21:32 +0400

    created a folder and added all files

---
+++ lab3_gallery/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Галерея</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1,
+    shrink-to-fit=no">
+    <link rel="stylesheet" href="styles/mainStyle.css">
+    <link rel="stylesheet" href="styles/galleryStyle.css">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@700&family=Rubik+Wet+Paint&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@700&family=Pacific&family=Rubik+Wet+Paint&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js"></script>
+    <script type="text/javascript" src="scripts/main.js"></script>
+</head>
+<body>
+<header class="header">
+    <div class="container">
+        <div class="nav">
+            <a href="#">
+                <img src="images/NX.png" alt="Logo" class="logo">
+            </a>
+            <ul class="menu">
+                <li>
+                    <a href="#">Главная</a>
+                </li>
+                <li>
+                    <a href="#">Мои работы</a>
+                </li>
+                <li>
+                    <a href="index.php" style="border-bottom: 3px solid red;">Галерея</a>
+                </li>
+                <li>
+                    <a href="#">О себе</a>
+                </li>
+                <li>
+                    <a href="#">Контакты</a>
+                </li>
+            </ul>
+            <ul class="sign-up">
+                <li>
+                    <a href="#">Регистрация</a>
+                </li>
+                <li><span>|</span></li>
+                <li>
+                    <a href="#">Войти</a>
+                </li>
+                <li>
+                    <button class="menu-open">
+                        <img src="images/menu.svg" alt="open-menu">
+                    </button>
+                </li>
+            </ul>
+        </div>
+        <div class="header-content-gallery">
+            <h1 class="text-title-gallery">Галерея</h1>
+            <div class="copyright">
+                Этот сайт разработан в рамках выполнения лабораторной работы в 2022 г.
+            </div>
+        </div>
+</header>
+</body>
+</html>
