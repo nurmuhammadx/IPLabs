d8c2af353125c95d494bd3f7292140ba9047a58c
Nurmuhammad <98839225+nurmuhammadx@users.noreply.github.com>
2022-04-16 19:35:56 +0400

    added gallery style and script

---
+++ lab2_site/scripts/galleryScript.js
@@ -0,0 +1,57 @@
+const prev = document.getElementById('btn-prev'),
+      next = document.getElementById('btn-next'),
+      slides = document.querySelectorAll('.slide'),
+      dots = document.querySelectorAll('.dot');
+
+let index = 0;
+
+const activeSlide = n => {
+    for(slide of slides) {
+        slide.classList.remove('active');
+    }
+    slides[n].classList.add('active');
+}
+
+const activeDot = n => {
+    for(let dot of dots) {
+        dot.classList.remove('active');
+    }
+    dots[n].classList.add('active');
+}
+
+const activeFunc = (ind) => {
+    activeSlide(index);
+    activeDot(index);
+}
+
+const nextSlide = () => {
+    if(index === slides.length - 1) {
+        index = 0;
+        activeFunc(index);
+    } else {
+        index++;
+        activeFunc(index);
+    }
+}
+
+const prevSlide = () => {
+    if(index === 0) {
+        index = slides.length - 1;
+        activeFunc(index);
+    } else {
+        index--;
+        activeFunc(index);
+    }
+}
+
+dots.forEach((item, indexDot) => {
+    item.addEventListener('click', () => {
+        index = indexDot;
+        activeFunc(index);
+    })
+})
+
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
+
+setInterval(nextSlide, 3000);
