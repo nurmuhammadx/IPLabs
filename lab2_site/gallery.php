a498959e9f6972b57ef4622f420b6941f00695e8
Nurmuhammad <98839225+nurmuhammadx@users.noreply.github.com>
2022-05-10 18:45:26 +0400

    new gallery

--- lab2_site/gallery.php
+++ lab2_site/gallery.php
@@ -2,14 +2,16 @@
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <title>Галерея</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="styles/mainStyle.css">
     <link rel="stylesheet" href="styles/galleryStyle.css">
-    <link rel="stylesheet" href="styles/animate.css">
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@700&family=Rubik+Wet+Paint&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@700&family=Pacific&family=Rubik+Wet+Paint&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+    <script src="scripts/jquery-3.6.0.min.js"></script>
+    <script src="scripts/main.js"></script>
+    <script src="scripts/galleryScript.js"></script>
+    <title>Галерея</title>
 </head>
 <body>
 <header class="header">
@@ -20,19 +22,19 @@
             </a>
             <ul class="menu">
                 <li>
-                    <a href="index.php">Главная</a>
+                    <a href="#">Главная</a>
                 </li>
                 <li>
-                    <a href="myworks.php">Мои работы</a>
+                    <a href="#">Мои работы</a>
                 </li>
                 <li>
-                    <a href="gallery.php" style="border-bottom: 3px solid red;">Галерея</a>
+                    <a href="index.php" style="border-bottom: 3px solid red;">Галерея</a>
                 </li>
                 <li>
-                    <a href="about.php">О себе</a>
+                    <a href="#">О себе</a>
                 </li>
                 <li>
-                    <a href="contacts.php">Контакты</a>
+                    <a href="#">Контакты</a>
                 </li>
             </ul>
             <ul class="sign-up">
@@ -52,28 +54,20 @@
         </div>
         <div class="header-content-gallery">
             <h1 class="text-title-gallery">Галерея</h1>
-            <div class="flex-wrapper">
-                <ul id="photos">
-                    <li><img src="images/gallery/1.jpg" alt=""></li>
-                    <li><img src="images/gallery/2.jpg" alt=""></li>
-                    <li><img src="images/gallery/3.jpg" alt=""></li>
-                    <li><img src="images/gallery/4.jpg" alt=""></li>
-                    <li><img src="images/gallery/5.jpg" alt=""></li>
-                </ul>
-                <div class="arrow previous">
+            <div class="gallery-box">
+                <div class="btn-left" onclick="leftClickImage()">
                     <img src="images/gallery/arr-left.png" alt="">
                 </div>
-                <div class="arrow next">
+                <div id="mainImage"></div>
+                <div class="btn-right" onclick="rightClickImage()">
                     <img src="images/gallery/arr-right.png" alt="">
                 </div>
             </div>
-            <div class="copyright">
-                Этот сайт разработан в рамках выполнения лабораторной работы в 2022 г.
-            </div>
         </div>
+        <div class="copyright">
+            Этот сайт разработан в рамках выполнения лабораторной работы в 2022 г.
+        </div>
+    </div>
 </header>
-<script src="https://code.jquery.com/jquery-3.6.0.slim.min.js"></script>
-<script type="text/javascript" src="scripts/main.js"></script>
-<script type="text/javascript" src="scripts/galleryScript.js"></script>
 </body>
 </html>
