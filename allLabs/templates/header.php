b9ca1eafaea21a1b5c32f2d6c61b4613eb6f9b37
Nurmuhammad <98839225+nurmuhammadx@users.noreply.github.com>
2022-05-15 20:51:17 +0400

    changed the folder name

--- allLabs/templates/header.php
+++ allLabs/templates/header.php
@@ -1,28 +1,28 @@
 <div class="nav">
-    <a href="/lab2_site/index.php">
-        <img src="/lab2_site/images/main/NX.png" alt="Logo" class="logo">
+    <a href="/allLabs/index.php">
+        <img src="/allLabs/images/main/NX.png" alt="Logo" class="logo">
     </a>
     <ul class="menu">
         <li>
             <a href="../index.php">Главная</a>
         </li>
         <li>
-            <a href="/lab2_site/pages/works.php">Мои работы</a>
+            <a href="/allLabs/pages/works.php">Мои работы</a>
         </li>
         <li>
-            <a href="/lab2_site/pages/gallery.php">Галерея</a>
+            <a href="/allLabs/pages/gallery.php">Галерея</a>
         </li>
         <li>
-            <a href="/lab2_site/pages/about.php">Обо мне</a>
+            <a href="/allLabs/pages/about.php">Обо мне</a>
         </li>
         <li>
-            <a href="/lab2_site/pages/contacts.php">Контакты</a>
+            <a href="/allLabs/pages/contacts.php">Контакты</a>
         </li>
     </ul>
     <ul class="sign-up">
         <li>
             <?php if (!isset($_SESSION["auth"])) {
-                echo "<a href='/lab2_site/pages/registration.php'>Регистрация</a>";
+                echo "<a href='/allLabs/pages/registration.php'>Регистрация</a>";
             } ?>
         </li>
         <li>
@@ -32,22 +32,22 @@
         </li>
         <li>
             <?php if (!isset($_SESSION["auth"])) {
-                echo "<a href='/lab2_site/pages/authorization.php'>Войти</a>";
+                echo "<a href='/allLabs/pages/authorization.php'>Войти</a>";
             } ?>
         </li>
         <?php if (isset($_SESSION["auth"])) {
             if(isset($_SESSION["username"]) && $_SESSION["username"] != 0) {
-                echo "<li><a href='/lab2_site/pages/profile.php'>" . $_SESSION["username"] . "</a></li>";
+                echo "<li><a href='/allLabs/pages/profile.php'>" . $_SESSION["username"] . "</a></li>";
             }
             if (isset($_SESSION['user_type']) && $_SESSION['user_type'] == 0) {
-                echo "<li><a href='/lab2_site/pages/admin.php'><h3 class='admin-a'>Administrator</h3></a></li>";
+                echo "<li><a href='/allLabs/pages/admin.php'><h3 class='admin-a'>Administrator</h3></a></li>";
             }
-            echo "<li><a href='/lab2_site/server/signout.php'>Выход</a></li>";
+            echo "<li><a href='/allLabs/server/signout.php'>Выход</a></li>";
             }
             ?>
         <li>
             <button class="menu-open">
-                <img src="/lab2_site/images/icons/menu.svg" alt="open-menu">
+                <img src="/allLabs/images/icons/menu.svg" alt="open-menu">
             </button>
         </li>
     </ul>
